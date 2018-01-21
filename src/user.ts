aa2814cb89069795726779c896e585f5fb88836c
Elo <elo@laposte.net>
2018-01-21 11:08:01 +0100

    ajout dossier src contenant classe user

---
+++ src/user.ts
@@ -0,0 +1,10 @@
+﻿class User {
+	let id: number;
+    let lastName: string;
+    let firstName: string;
+    let age: number;
+    function displayInfos(): string {
+        let informations: string = id + lastName + firstName + age;
+        return informations;
+    }
+}
