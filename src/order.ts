675258c1035aa9717f4bc2f2cd0113482b009129
Elo <elo@laposte.net>
2018-01-21 11:20:26 +0100

    ajout order.ts dans dossier src

---
+++ src/order.ts
@@ -0,0 +1,8 @@
+﻿class Order {
+	let userId: number;
+    let productReference: string;
+    let quantity: number;
+    function process(): void {
+        console.log(" Order has been handled");
+    }
+}
